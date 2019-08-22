2bb015cd5702afecafa10381f99f24696cac0519
Fabio Moraes <fabio@elaboredigital.com.br>
2019-08-21 21:08:56 -0300

    Em andamento

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialect: 'postgres', // Podendo acessar mysql - sql
+  host: 'localhost',
+  username: 'postgres',
+  password: 'docker',
+  database: 'gobarber',
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,16 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => res.json({ message: 'Hello World' }));
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Fabio Moraes',
+    email: 'fabio@elaboredigital.com.br',
+    password_hash: '312312312312',
+  });
+
+  return res.json(user);
+});
 
 export default routes;
