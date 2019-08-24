cfb211760d5385e35c4753ad59dd0a55b8954eda
Fabio Moraes <fabio@elaboredigital.com.br>
2019-08-24 18:23:03 -0300

    Arquivos 24-08-2019

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -25,6 +25,11 @@ class User extends Model {
     return this;
   }
 
+  // Model de ralacionamento com outra tabela
+  static associate(models) {
+    this.belongsTo(models.File, { foreignKey: 'avatar_id', as: 'avatar' });
+  }
+
   checkPassword(password) {
     return bcrypt.compare(password, this.password_hash);
   }
